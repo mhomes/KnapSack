5bae9494f751d27d422a4ea5fa4c79134dccd147
AllenBurris <32074597+AllenBurris@users.noreply.github.com>
2018-09-26 14:39:08 -0400

    bugs fixed
    preparing to press F

--- GreedyKnapSack/GreedyKnapSack/KnapSackMain.cpp
+++ GreedyKnapSack/GreedyKnapSack/KnapSackMain.cpp
@@ -11,29 +11,28 @@
 //#include "Loot.cpp"
 using namespace std;
 
-class Loot {
+double findRatio(int w, int v);
 
-public:
+struct Loot {
 
-	static string name;
-	static int weight;
-	static int value;
-	static double ratio;
+	string name;
+	int weight;
+	int value;
+	double ratio;
 
 };
 
-class Node {
+/*
+struct Node {
 
-public:
 	Loot* heldItem;
 	Node* LHNode;
 	Node* RHNode;
 	Node* parent;
 
-	//	Node(Loot item, Node left, Node right, Node mom) {
+}; */
 
-		//}
-};
+
 
 int main() {
 
@@ -45,22 +44,23 @@ int main() {
 	fileIn >> numGems >> bagSize;
 
 	// create Loot objects for each item
-	Loot test[] = new Loot[30];
+	Loot * test = new Loot[30];
 	//std::vector<Loot> testArray;
 	for (int i = 0; i < numGems; i++) {
 		string n;
 		int w, v;
-		fileIn.get(n);
+		fileIn >> n;
 		fileIn >> w >> v;
 		test[i].name = n;
 		test[i].weight = w;
 		test[i].value = v;
 		test[i].ratio = findRatio(w, v);
-		cout << testArray[i].name << endl;
+		//cout << test[i].name << endl;
 	}
 
-	for (int i = 0; i < numGems; i++)
-		//delete testArray[i];
+	for (int i = 0; i < numGems; i++) {
+		//delete &test[i];
+	}
 
 	// done - read file in
 	//create object for it
