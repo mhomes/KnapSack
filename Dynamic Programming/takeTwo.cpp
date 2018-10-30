129d37b3463036a65f90c3f989811c5a817fe616
AllenBurris <32074597+AllenBurris@users.noreply.github.com>
2018-10-29 21:55:26 -0400

    well.... shit

--- Dynamic Programming/takeTwo.cpp
+++ Dynamic Programming/takeTwo.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include <fStream>
+#include <fstream>
 #include <cstring>
 
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 struct Loot {
 
 	string name;
-	int weiht, value;
+	int weight, value;
 	double ratio;
 
 	void print() {
@@ -16,25 +16,31 @@ struct Loot {
 	}
 };
 
-int fill(int i, int w, int matrix[][], Loot holdThis[]) {
+int fill(int i, int w, int **matrix, Loot *holdThis) {
 
-	if (holdThis[i].weight > w)
-		return fill(i - 1, w, matrix, holdThis);
-	else if(holdThis.weight <= w)
-
+	if (i == 0 || w == 0)
+		return 0;
+	if (w < holdThis[i].weight) 
+		matrix[i][w] = fill(i - 1, w, matrix, holdThis);
+	else
+		matrix[i][w] = max(fill(i - 1, w, matrix, holdThis), (fill(i - 1, w - holdThis[i].weight, matrix, holdThis) + holdThis[i].value));
+	
 }
 
 int main() {
 
-	string n; 
-	int numGems, bagSize, w, v; 
+	string n;
+	int numGems, bagSize, w, v;
 
-	ifstream fileIn; 
+	ifstream fileIn;
 	fileIn.open("TestFile.txt");
 	fileIn >> numGems >> bagSize;
 
-	int matrix[numGems + 1][bagSize + 1];
-	Loot holdThis[numGems + 1];
+	int** matrix = new int * [numGems + 1];
+	for (int i = 0; i <= numGems; i++)
+		matrix[i] = new int[bagSize+1];
+
+	Loot* holdThis = new Loot[numGems + 1];
 
 	for (int w = 0; w <= bagSize; w++)
 		matrix[0][w] = 0;
@@ -47,7 +53,41 @@ int main() {
 		holdThis[i].value = v;
 	}
 
-	fill(numGems + 1, bagSize + 1, matrix, holdThis);
+	fill(numGems, bagSize, matrix, holdThis);
+
+	int weight = 0, value = 0;
+	int numContents = 0;
+	int contents[numGems+1];
+
+	for (int i = numGems; i > 0; i--) {
+		cout <<"i = "<< i << endl;
+		for (int w = bagSize; w > 0; w--) {
+			cout << "w = " << i << endl;
+			if (matrix[i][w] = matrix[i - 1][w])
+				i--;
+			else {
+				w = w - holdThis[i].weight;
+				weight += holdThis[i].weight;
+				value += holdThis[i].value;
+				contents[numContents] = i;
+				numContents++;
+				cout << "i-1 ";
+				i--;
+				cout << i << endl;
+				//cout << i << " " << w << endl;
+			}
+		}
+	}
+	
+	cout << "fuck commit dead" << endl;
+	cout << numContents << endl;
+	cout << weight << endl;
+	cout << value << endl;
+	for (int i = 0; i < numContents; i++) {
+		cout << holdThis[contents[i]].name << " ";
+		cout << holdThis[contents[i]].value << " ";
+		cout << holdThis[contents[i]].weight << endl;
+	}
 
 	return 0;
 }
